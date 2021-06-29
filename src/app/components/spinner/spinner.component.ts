10702f71f2a2a006ccf244cd2cde6a3f04508cf0
JITENPANDEY <49372019+JITENPANDEY@users.noreply.github.com>
2021-06-29 23:27:50 +0530

    Add files via upload

---
+++ src/app/components/spinner/spinner.component.ts
@@ -0,0 +1,32 @@
+
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Subject } from 'rxjs';
+import { SpinnerService } from 'src/app/services/spinner/spinner.service';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss']
+})
+export class SpinnerComponent implements OnInit {
+
+  showSpinner = false;
+  isLoading: Subject<boolean> = this.spinnerService.isLoading;
+
+  constructor(private spinnerService: SpinnerService, private cdRef: ChangeDetectorRef) {
+
+  }
+
+  ngOnInit() {
+    this.init();
+  }
+
+  init() {
+
+    this.spinnerService.getSpinnerObserver().subscribe((status) => {
+      this.showSpinner = (status === 'start');
+      this.cdRef.detectChanges();
+    });
+  }
+
+}
